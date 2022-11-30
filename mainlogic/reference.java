fe0ed17a8fcc73cb4d582fe0036675e631cf54e4
clmtwu <inchiawu@gmail.com>
2022-11-29 21:22:42 -0800

    answer arraylist with score calculation

---
+++ mainlogic/reference.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class reference {
+    ArrayList<String> answer = new ArrayList<String>(Arrays.asList
+    ("a", "b ", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x" , "y", "z"));
+    final double accuracy_portion = 0.3;
+    final double combo_portion = 0.7;
+    double total_score = 1000000 * ((accuracy * accuracy_portion) + (combo / answer.size() * combo_portion)); 
+}
