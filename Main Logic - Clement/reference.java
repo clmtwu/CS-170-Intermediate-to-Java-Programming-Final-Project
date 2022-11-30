1a98fdd495610544273dc96363024a2e7fc26773
clmtwu <inchiawu@gmail.com>
2022-11-29 21:26:50 -0800

    updated names

--- Main Logic - Clement/reference.java
+++ Main Logic - Clement/reference.java
@@ -4,5 +4,9 @@ public class reference {
     ("a", "b ", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x" , "y", "z"));
     final double accuracy_portion = 0.3;
     final double combo_portion = 0.7;
-    double total_score = 1000000 * ((accuracy * accuracy_portion) + (combo / answer.size() * combo_portion)); 
+    double total_score;
+
+    public void updateScore(double accuracy, double combo) {
+        total_score = 1000000 * ((accuracy * accuracy_portion) + (combo / answer.size() * combo_portion)); 
+    }
 }
