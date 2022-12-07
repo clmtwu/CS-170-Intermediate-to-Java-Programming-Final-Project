1363cbcf154ec7ab4deb5056585255221855fa20
Clement Wu <inchiawu@gmail.com>
2022-12-07 15:54:12 -0800

    learning mode that barely works

--- Alphabet.java
+++ Alphabet.java
@@ -5,52 +5,83 @@ import java.awt.Dimension;
 import javax.swing.border.EmptyBorder;
 
 public class Alphabet {
-    JButton A = new JButton("A");
-    JButton B = new JButton("B");
-    JButton C = new JButton("C");
-    JButton D = new JButton("D");
-    JButton E = new JButton("E");
-    JButton F = new JButton("F");
-    JButton G = new JButton("G");
-    JButton H = new JButton("H");
-    JButton I = new JButton("I");
-    JButton J = new JButton("J");
-    JButton K = new JButton("K");
-    JButton L = new JButton("L");
-    JButton M = new JButton("M");
-    JButton N = new JButton("N");
-    JButton O = new JButton("O");
-    JButton P = new JButton("P");
-    JButton Q = new JButton("Q");
-    JButton R = new JButton("R");
-    JButton S = new JButton("S");
-    JButton T = new JButton("T");
-    JButton U = new JButton("U");
-    JButton V = new JButton("V");
-    JButton W = new JButton("W");
-    JButton X = new JButton("X");
-    JButton Y = new JButton("Y");
-    JButton Z = new JButton("Z");
+    static JButton A = new JButton("A");
+    static JButton B = new JButton("B");
+    static JButton C = new JButton("C");
+    static JButton D = new JButton("D");
+    static JButton E = new JButton("E");
+    static JButton F = new JButton("F");
+    static JButton G = new JButton("G");
+    static JButton H = new JButton("H");
+    static JButton I = new JButton("I");
+    static JButton J = new JButton("J");
+    static JButton K = new JButton("K");
+    static JButton L = new JButton("L");
+    static JButton M = new JButton("M");
+    static JButton N = new JButton("N");
+    static JButton O = new JButton("O");
+    static JButton P = new JButton("P");
+    static JButton Q = new JButton("Q");
+    static JButton R = new JButton("R");
+    static JButton S = new JButton("S");
+    static JButton T = new JButton("T");
+    static JButton U = new JButton("U");
+    static JButton V = new JButton("V");
+    static JButton W = new JButton("W");
+    static JButton X = new JButton("X");
+    static JButton Y = new JButton("Y");
+    static JButton Z = new JButton("Z");
 
     final static int alphabetsize = 26;
     final static int titlefont = 25;
     final static int bodyfont = 15;
 
-	ArrayList<Integer> parent = new ArrayList<Integer>(alphabetsize);
+	static ArrayList<Integer> parent = new ArrayList<Integer>(alphabetsize);
 	static ArrayList<JButton> alphabet = new ArrayList<JButton>(alphabetsize);
 
 	FlowLayout FrameLayout = new FlowLayout(FlowLayout.CENTER);
     FlowLayout LMFrameLayout = new FlowLayout(FlowLayout.CENTER); 
 
-    Dimension LongJButtonSize = new Dimension (300, 100);
+    static Dimension LongJButtonSize = new Dimension (300, 100);
+    static Dimension StandardJButtonSize = new Dimension (200, 50);
 
     EmptyBorder TitleBorder = new EmptyBorder (50, 300, 50, 300);
     EmptyBorder RegularTextBorder = new EmptyBorder (15, 200, 15, 200);
 
-    public static void setUp() {
-        for (char i = 65; i <= 90; i++) {
-			String temp = Character.toString(i);
-			alphabet.add(new JButton(temp));
-		}
+    public static void setUpAlphabet() {
+        alphabet.add(A);
+        alphabet.add(B);
+        alphabet.add(C);
+        alphabet.add(D);
+        alphabet.add(E);
+        alphabet.add(F);
+        alphabet.add(G);
+        alphabet.add(H);
+        alphabet.add(I);
+        alphabet.add(J);
+        alphabet.add(K);
+        alphabet.add(L);
+        alphabet.add(M);
+        alphabet.add(N);
+        alphabet.add(O);
+        alphabet.add(P);
+        alphabet.add(Q);
+        alphabet.add(R);
+        alphabet.add(S);
+        alphabet.add(T);
+        alphabet.add(U);
+        alphabet.add(V);
+        alphabet.add(W);
+        alphabet.add(X);
+        alphabet.add(Y);
+        alphabet.add(Z);
     }
+
+    public static void setUpInteger() {
+        for (int i = 0; i < alphabetsize; i++) {
+            parent.add(i);
+        }
+    }
+
+    
 }
--- LearningMode.java
+++ LearningMode.java
@@ -3,9 +3,12 @@ import java.awt.event.*; //importing awt for customization
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class LearningMode extends Alphabet {
     JFrame LMFrame = new JFrame();
+    JFrame AlphabetPrint = new JFrame();
 
     JButton LMConfirm = new JButton ("Okay, I got it!");
     JButton LMRepeat = new JButton ("Please repeat one more time!"); //this one is correct
@@ -13,35 +16,39 @@ public class LearningMode extends Alphabet {
     JLabel LMPrompt = new JLabel ("Here is the alphabet in order!", SwingConstants.CENTER);
 
     public void setup() {
-        Alphabet.setUp();
-        LMFrame.setSize(1000, 1000); 
-        LMFrame.setTitle("Learning Mode - Alphabet Game");
+        Alphabet.setUpAlphabet();
+        LMFrame.setSize(300, 300); 
+        LMFrame.setTitle("Learning Mode - Control Panel!");
         LMFrame.setLayout(LMFrameLayout);
         LMFrame.setResizable(false);
-        LMFrame.setLocationRelativeTo(null);
-
         LMFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // default action when closed is to stop
+
+        AlphabetPrint.setSize(500, 1000); 
+        AlphabetPrint.setTitle("Learning Mode - Alphabet!");
+        AlphabetPrint.setResizable(false);
+        AlphabetPrint.setLocationRelativeTo(null);
+        AlphabetPrint.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // default action when closed is to stop
 
         LMConfirm.setForeground(Color.GREEN);
         LMRepeat.setForeground(Color.RED);
 
         LMPrompt.setBorder(TitleBorder);
 
-        LMConfirm.setPreferredSize(LongJButtonSize);
-        LMRepeat.setPreferredSize(LongJButtonSize);
+        LMConfirm.setPreferredSize(StandardJButtonSize);
+        LMRepeat.setPreferredSize(StandardJButtonSize);
     }
 
     public LearningMode() {
         setup();
         LMFrame.setVisible(true);
-		LMFrame.add(LMPrompt);
-        for (int i = 0; i < parent.size(); i++) {
-            LMFrame.add(alphabet.get(i));
-        }
+		LMFrame.add(LMPrompt, BorderLayout.CENTER);
         LMFrame.add(LMConfirm);
-        LMFrame.add(LMRepeat);
+        //LMFrame.add(LMRepeat);
+        printAlphabetinOrder();
         LMConfirm.addActionListener((java.awt.event.ActionListener) new ActionListener() { //creating action listener for submit button
             public void actionPerformed(ActionEvent e){ //method of execution
+                AlphabetPrint.setVisible(false);
+                AlphabetPrint.dispose();
                 LMFrame.setVisible(false); //you can't see me!
                 LMFrame.dispose(); //Destroy the JFrame object
                 new Frame();
@@ -54,6 +61,15 @@ public class LearningMode extends Alphabet {
                 new LearningMode();
             }
         });
+        
+    }
+
+    public void printAlphabetinOrder() {
+        AlphabetPrint.setLayout(new GridLayout(alphabet.size(), 1));
+        for (int i = 0; i < alphabet.size(); i++) {
+            AlphabetPrint.add(alphabet.get(i), BorderLayout.CENTER);
+        }
+        AlphabetPrint.setVisible(true);
     }
 
     public static void main(String[] args) {
