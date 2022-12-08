edb11c789eb60b6f3389e6621f19f2b2ff482405
HengjinMao <119469844+HengjinMao@users.noreply.github.com>
2022-12-07 18:51:04 -0800

    Update sound.java

--- sound.java
+++ sound.java
@@ -9,11 +9,29 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class sound {
 
 
 
 	public static void RunMusic(String path) throws LineUnavailableException {
+	
+		
+		
 		try {
 			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
 			Clip clip = AudioSystem.getClip();
@@ -28,4 +46,27 @@ public class sound {
 
 	}
 
+	public static void main (String[] args) throws LineUnavailableException {
+		
+		File file = new File("windows10.wav");
+		
+		try {
+			AudioInputStream input = AudioSystem.getAudioInputStream(file);
+			Clip clip = AudioSystem.getClip();
+			clip.open(input);
+			clip.start();
+			
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
 }
